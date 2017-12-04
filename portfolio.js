442fc9f5d815a8801f203e0a79323e57707410ad
jaronanderson <jaron.anderson@students.svu.edu>
2017-12-04 10:54:01 -0500

    added lightbox to portfolio images

--- portfolio.js
+++ portfolio.js
@@ -11,3 +11,26 @@ var quote = quotes[number];
 
 var elName = document.getElementById('testimonial');
 elName.textContent = quote;
+
+/*     Jquery     */
+
+$(document).ready(function(){
+
+	$(".portfolio img").on("click", function() {
+
+		var lightbox = $('<div id= "lightbox"></div>');
+		$("body").append(lightbox);
+
+
+		var lightboximg = $("<img/>");
+		lightbox.append(lightboximg);
+		
+		var imgsrc = $(this).attr("src");
+		lightboximg.attr('src', imgsrc);
+
+		lightbox.on("click", function(){
+			this.remove();
+		});
+
+	});
+});
