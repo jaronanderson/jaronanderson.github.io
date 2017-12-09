45e6541525715bfb469dd717877eddd896162bad
jaronanderson <jaron.anderson@students.svu.edu>
2017-12-09 10:33:45 -0500

    changed index page

---
+++ index.js
@@ -0,0 +1,24 @@
+<div class="dropdown show">
+  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown link
+  </a>
+  </div>
+
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <a class="dropdown-item" href="index.html">Assignment Home Page</a>
+    <a class="dropdown-item" href="biopage.html">Bio Page</a>
+    <a class="dropdown-item" href="contactpage.html">Contact Form</a>
+    <a class="dropdown-item" href="">Site-wide CSS</a>
+    <a class="dropdown-item" href="portfolio.html">Portfolio Page</a>
+    <a class="dropdown-item" href="howtoarticle.html">How-to-Article</a>
+    <a class="dropdown-item" href="responsivelandingpage.html">Responsive Landing Page</a>
+    <a class="dropdown-item" href="">Dynamic Copyright</a>
+    <a class="dropdown-item" href="">Random Customer Testamonial</a>
+    <a class="dropdown-item" href="selectingelements.html">Selecting Elements</a>
+    <a class="dropdown-item" href="">Scripted Product List</a>
+    <a class="dropdown-item" href="">Image Lightbox</a>
+    <a class="dropdown-item" href="">Bootstrap Page</a>
+
+  
+  </div>
+</div>
